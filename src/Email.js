5c2c35909a1f6713c4607965b4b7f10f26787e7c
birdsong5 <108277183+birdsong5@users.noreply.github.com>
2023-12-17 13:32:17 +0100

    updated Email component

--- src/Email.js
+++ src/Email.js
@@ -1,10 +1,13 @@
+import "./main.css";
+
 const Email = () => {
   return (
     <div className="summary-output">
       <p className="summary-title">GENERATED EMAILS</p>
       <div className="summary-text"> text of the email </div>
-      <div>
-        <button>Send email</button>
+      <div className="email-send">
+        <div>Verify and send the email </div>
+        <button className="send-button">Send email</button>
       </div>
     </div>
   );
